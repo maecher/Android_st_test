eacf1eba87ee0534af5d50b74e81ba5abb63cd1e
sgcheol <93517114+sgcheol@users.noreply.github.com>
2023-03-11 18:56:21 +0900

    Second commit : part3

--- app/src/main/java/com/example/capstone_study_1/MainActivity.java
+++ app/src/main/java/com/example/capstone_study_1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.capstone_study_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,23 +10,26 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et_id;
-    Button btn_test;
-
-
+    private Button btn_test;
+    private EditText et_test;
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_id = findViewById(R.id.et_id);
+        et_test = findViewById(R.id.et_test);
+
         btn_test = findViewById(R.id.btn_id);
 
         btn_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                et_id.setText("홍드로이드");
+                str = et_test.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str", str);
+                startActivity(intent); // 액티비티 이동.
             }
         });
     }
