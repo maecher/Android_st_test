56ff6b3762f4657bcd5a5975a862e6023f46c057
sgcheol <93517114+sgcheol@users.noreply.github.com>
2023-03-11 20:09:13 +0900

    Third commit : part4

--- app/src/main/java/com/example/capstone_study_1/MainActivity.java
+++ app/src/main/java/com/example/capstone_study_1/MainActivity.java
@@ -7,12 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btn_test;
     private EditText et_test;
     private String str;
+    ImageView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
                 intent.putExtra("str", str);
                 startActivity(intent); // 액티비티 이동.
+            }
+        });
+
+        test = (ImageView) findViewById(R.id.test);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText((getApplicationContext()),"홍드로이드" , Toast.LENGTH_SHORT).show();
             }
         });
     }
