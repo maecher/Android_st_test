ca64b3562fa85e05f4481ee3fc411ec4ce2251b3
sgcheol <93517114+sgcheol@users.noreply.github.com>
2023-03-11 20:50:35 +0900

    Fourth commit : part6

--- app/src/main/java/com/example/capstone_study_1/MainActivity.java
+++ app/src/main/java/com/example/capstone_study_1/MainActivity.java
@@ -5,44 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_test;
-    private EditText et_test;
-    private String str;
-    ImageView test;
+    private ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_test = findViewById(R.id.et_test);
+        list = (ListView) findViewById(R.id.list);
 
-        btn_test = findViewById(R.id.btn_id);
+        List<String> data = new ArrayList<>();
 
-        btn_test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                str = et_test.getText().toString();
-                Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                intent.putExtra("str", str);
-                startActivity(intent); // 액티비티 이동.
-            }
-        });
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,data);
+        list.setAdapter(adapter);
 
-        test = (ImageView) findViewById(R.id.test);
-        test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText((getApplicationContext()),"홍드로이드" , Toast.LENGTH_SHORT).show();
-            }
-        });
+        data.add("홍드로이드");
+        data.add("홍드로이드");
+        data.add("홍드로이드");
+        adapter.notifyDataSetChanged(); // 바뀐 상태 저장.
     }
 
 
